9929eb2a55137d782f93ffddeb622c121d804ea6
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-04-04 10:26:45 -0400

    commit tree 9fdf850c05960188b4f937d09bedc5475c2b270a

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ApplicationRecord
+  # Author Basic validations is expected to validate that :name cannot be empty/falsy
+  validates :name, presence: true
+
+  # Author Basic validations is expected to validate that :name is case-sensitively unique
+  validates_uniqueness_of :name, case_sensitive: true
+
+  # Author Basic validations is expected to validate that the length of :phone_number is 10
+  validates :phone_number, length: { is: 10 }
 end
